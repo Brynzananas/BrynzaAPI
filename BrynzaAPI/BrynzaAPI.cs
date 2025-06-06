630e4ff8572f02e8071f23d49253e2ce2c526142
Brynzananas <brynzananas@gmail.com>
2025-06-06 19:38:38 +0600

    Add project files.

---
+++ BrynzaAPI/BrynzaAPI.cs
@@ -0,0 +1,186 @@
+﻿using BepInEx;
+using System.Security;
+using System.Security.Permissions;
+using R2API;
+using MonoMod.Cil;
+using RoR2.Skills;
+using Mono.Cecil.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+using RoR2.CameraModes;
+using RoR2.UI;
+using RoR2.ConVar;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+[assembly: HG.Reflection.SearchableAttribute.OptInAttribute]
+[module: UnverifiableCode]
+#pragma warning disable CS0618
+#pragma warning restore CS0618
+namespace BrynzaAPI
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [BepInDependency(R2API.CharacterBodyAPI.PluginGUID)]
+    [System.Serializable]
+    public class BrynzaAPI : BaseUnityPlugin
+    {
+        public const string ModGuid = "com.brynzananas.brynzaapi";
+        public const string ModName = "Brynza API";
+        public const string ModVer = "1.0.0";
+        public void Awake()
+        {
+            IL.RoR2.Skills.SkillDef.OnFixedUpdate += SkillDef_OnFixedUpdate;
+            IL.RoR2.Skills.SkillDef.OnExecute += SkillDef_OnExecute;
+            IL.RoR2.UI.CrosshairManager.UpdateCrosshair += CrosshairManager_UpdateCrosshair1;
+            IL.RoR2.CameraModes.CameraModePlayerBasic.UpdateInternal += CameraModePlayerBasic_UpdateInternal;
+            IL.RoR2.CameraModes.CameraModePlayerBasic.CollectLookInputInternal += CameraModePlayerBasic_CollectLookInputInternal;
+        }
+
+        private void CameraModePlayerBasic_CollectLookInputInternal(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            ILLabel iLLabel = null;
+            if (c.TryGotoNext(
+                    x => x.MatchLdsfld<CameraRigController>(nameof(CameraRigController.enableSprintSensitivitySlowdown)),
+                    x => x.MatchCallvirt<BoolConVar>("get_value"),
+                    x => x.MatchBrfalse(out iLLabel)
+                ))
+            {
+                c.Emit(OpCodes.Ldarg_2);
+                c.Emit<CameraModeBase.CameraModeContext>(OpCodes.Ldflda, nameof(CameraModeBase.CameraModeContext.targetInfo));
+                c.Emit<CameraModeBase.TargetInfo>(OpCodes.Ldfld, nameof(CameraModeBase.TargetInfo.body));
+                c.EmitDelegate<Func<CharacterBody, bool>>((cb) =>
+                {
+                    return cb ? cb.HasModdedBodyFlag(Assets.SprintAllTime) : false;
+
+                });
+                c.Emit(OpCodes.Brtrue_S, iLLabel);
+            }
+            else
+            {
+                Debug.LogError(il.Method.Name + " IL Hook failed!");
+            }
+        }
+
+        private static void SkillDef_OnExecute(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            ILLabel iLLabel = null;
+            if (c.TryGotoNext(
+                    x => x.MatchLdarg(0),
+                    x => x.MatchLdfld<SkillDef>("cancelSprintingOnActivation"),
+                    x => x.MatchBrfalse(out iLLabel)
+                ))
+            {
+                c.Emit(OpCodes.Ldarg_1);
+                c.EmitDelegate<Func<GenericSkill, bool>>((cb) =>
+                {
+                    return cb.characterBody.HasModdedBodyFlag(Assets.SprintAllTime);
+
+                });
+                c.Emit(OpCodes.Brtrue_S, iLLabel);
+            }
+            else
+            {
+                Debug.LogError(il.Method.Name + " IL Hook failed!");
+            }
+        }
+
+        private static void SkillDef_OnFixedUpdate(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            ILLabel iLLabel = null;
+            if (c.TryGotoNext(
+                    x => x.MatchLdarg(1),
+                    x => x.MatchCallvirt<GenericSkill>("get_characterBody"),
+                    x => x.MatchCallvirt<CharacterBody>("get_isSprinting"),
+                    x => x.MatchBrfalse(out iLLabel)
+                ))
+            {
+                c.Emit(OpCodes.Ldarg_1);
+                c.EmitDelegate<Func<GenericSkill, bool>>((cb) =>
+                {
+                    return cb.characterBody.HasModdedBodyFlag(Assets.SprintAllTime);
+
+                });
+                c.Emit(OpCodes.Brtrue_S, iLLabel);
+            }
+            else
+            {
+                Debug.LogError(il.Method.Name + " IL Hook failed!");
+            }
+        }
+        private static void CameraModePlayerBasic_UpdateInternal(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            ILLabel iLLabel = null;
+            if (
+                c.TryGotoNext(
+                    x => x.MatchLdarg(2),
+                    x => x.MatchLdflda<CameraModeBase.CameraModeContext>("targetInfo"),
+                    x => x.MatchLdfld<CameraModeBase.TargetInfo>("isSprinting"),
+                    x => x.MatchBrfalse(out iLLabel)
+                ))
+            {
+                //Debug.Log(c);
+                //Debug.Log(iLLabel?.Target);
+                c.Emit(OpCodes.Ldarg_2);
+                c.EmitDelegate(sus);
+                bool sus(ref CameraModeBase.CameraModeContext cameraModeContext)
+                {
+                    if (cameraModeContext.targetInfo.body != null)
+                    {
+                        return cameraModeContext.targetInfo.body.HasModdedBodyFlag(Assets.SprintAllTime);
+                    }
+                    else
+                    {
+                        return true;
+                    }
+
+                }
+                c.Emit(OpCodes.Brtrue_S, iLLabel);
+            }
+            else
+            {
+                Debug.LogError(il.Method.Name + " IL Hook failed!");
+            }
+        }
+        private static void CrosshairManager_UpdateCrosshair1(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            ILLabel iLLabel = null;
+            ILLabel iLLabel2 = null;
+            if (
+                c.TryGotoNext(
+                    x => x.MatchLdarg(0),
+                    x => x.MatchLdfld<CrosshairManager>("cameraRigController"),
+                    x => x.MatchCallvirt<CameraRigController>("get_hasOverride"),
+                    x => x.MatchBrtrue(out iLLabel)
+                )
+                &&
+                c.TryGotoNext(
+                    x => x.MatchLdarg(1),
+                    x => x.MatchCallvirt<CharacterBody>("get_isSprinting"),
+                    x => x.MatchBrtrue(out iLLabel2)
+                ))
+            {
+                c.Emit(OpCodes.Ldarg_1);
+                c.EmitDelegate<Func<CharacterBody, bool>>((cb) =>
+                {
+                    return cb.HasModdedBodyFlag(Assets.SprintAllTime);
+                });
+                c.Emit(OpCodes.Brtrue_S, iLLabel);
+            }
+            else
+            {
+                Debug.LogError(il.Method.Name + " IL Hook failed!");
+            }
+        }
+    }
+    public class Assets
+    {
+        public static CharacterBodyAPI.ModdedBodyFlag SprintAllTime = CharacterBodyAPI.ReserveBodyFlag();
+    }
+}
